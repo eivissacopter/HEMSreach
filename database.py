007a3bb62a690028f2464ec9b65df18a35ac7d37
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-07-03 00:44:23 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,15 @@
+# List of ADAC Luftrettung helicopter bases
+helicopter_bases = [
+    {"name": "Christoph 1 Munich", "lat": 48.3539, "lon": 11.7861},
+    {"name": "Christoph 2 Frankfurt", "lat": 50.0333, "lon": 8.5706},
+    {"name": "Christoph 3 Cologne", "lat": 50.8659, "lon": 7.1427},
+    # Add more bases here
+]
 
+# List of airports with IFR approaches
+airports = [
+    {"name": "Frankfurt Airport", "icao": "EDDF", "lat": 50.0379, "lon": 8.5622},
+    {"name": "Munich Airport", "icao": "EDDM", "lat": 48.3538, "lon": 11.7861},
+    {"name": "Berlin Brandenburg Airport", "icao": "EDDB", "lat": 52.3667, "lon": 13.5033},
+    # Add more airports here
+]
