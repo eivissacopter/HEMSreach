55af9cf5b75247215483a7d5ef13947cfcac1406
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-07-03 11:22:07 +0200

    Update adacwx.py

--- adacwx.py
+++ adacwx.py
@@ -56,7 +56,7 @@ def haversine(lon1, lat1, lon2, lat2):
     R = 6371.0  # Earth radius in kilometers
     lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
     dlon = lon2 - lon1
-    dlat = lat2 - lat1
+    dlat = lon2 - lat1
     a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     distance = R * c * 0.539957  # Convert to nautical miles
