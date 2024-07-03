775478dd29c377fe71f32d1d92c1ad0e4c35a260
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-07-03 13:04:44 +0200

    Update performance.py

--- performance.py
+++ performance.py
@@ -1,6 +1,6 @@
 # performance.py
 
 H145D2_PERFORMANCE = {
-    'cruise_speed_kt': 130,  # Cruising speed in knots
-    'fuel_burn_kgph': 260    # Fuel burn in kg per hour
+    'cruise_speed_kt': 120,  # Cruising speed in knots
+    'fuel_burn_kgph': 250    # Fuel burn in kg per hour
 }
