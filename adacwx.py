beba70f6845d8f0cfa73c17299cf9af3411082a9
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-07-04 16:31:50 +0200

    Update adacwx.py

--- adacwx.py
+++ adacwx.py
@@ -284,3 +284,22 @@ else:
 
     # Display the table
     st.table(df_reachable_airports)
+
+# performance.py
+
+H145D2_PERFORMANCE = {
+    'climb': {
+        'speed_kt': 90,           # Climb speed in knots
+        'fuel_burn_kgph': 250,    # Fuel burn during climb in kg per hour
+        'climb_rate_fpm': 900    # Climb rate in feet per minute
+    },
+    'cruise': {
+        'speed_kt': 115,          # Cruising speed in knots
+        'fuel_burn_kgph': 240     # Fuel burn in kg per hour
+    },
+    'descend': {
+        'speed_kt': 110,          # Descend speed in knots
+        'fuel_burn_kgph': 220,    # Fuel burn during descend in kg per hour
+        'descend_rate_fpm': 500  # Descend rate in feet per minute
+    }
+}
