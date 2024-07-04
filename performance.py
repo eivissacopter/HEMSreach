774186ed8c3bc10e99fb6a1a4b6e8a8daa766083
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-07-04 12:50:37 +0200

    Update and rename performance.py to performance_extended.py

--- performance.py
+++
@@ -1,6 +0,0 @@
-# performance.py
-
-H145D2_PERFORMANCE = {
-    'cruise_speed_kt': 105,  # Cruising speed in knots
-    'fuel_burn_kgph': 250    # Fuel burn in kg per hour
-}
---
+++ performance_extended.py
@@ -0,0 +1,18 @@
+# performance.py
+
+H145D2_PERFORMANCE = {
+    'climb': {
+        'speed_kt': 90,           # Climb speed in knots
+        'fuel_burn_kgph': 300,    # Fuel burn during climb in kg per hour
+        'climb_rate_fpm': 1500    # Climb rate in feet per minute
+    },
+    'cruise': {
+        'speed_kt': 105,          # Cruising speed in knots
+        'fuel_burn_kgph': 250     # Fuel burn in kg per hour
+    },
+    'descend': {
+        'speed_kt': 110,          # Descend speed in knots
+        'fuel_burn_kgph': 200,    # Fuel burn during descend in kg per hour
+        'descend_rate_fpm': 1000  # Descend rate in feet per minute
+    }
+}
