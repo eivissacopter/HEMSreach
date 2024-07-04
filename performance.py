04e67d204eb38c9338e579b9247265f7275e2085
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-07-04 12:57:05 +0200

    Update performance.py

--- performance.py
+++ performance.py
@@ -3,16 +3,16 @@
 H145D2_PERFORMANCE = {
     'climb': {
         'speed_kt': 90,           # Climb speed in knots
-        'fuel_burn_kgph': 300,    # Fuel burn during climb in kg per hour
-        'climb_rate_fpm': 1500    # Climb rate in feet per minute
+        'fuel_burn_kgph': 250,    # Fuel burn during climb in kg per hour
+        'climb_rate_fpm': 900    # Climb rate in feet per minute
     },
     'cruise': {
-        'speed_kt': 105,          # Cruising speed in knots
-        'fuel_burn_kgph': 250     # Fuel burn in kg per hour
+        'speed_kt': 115,          # Cruising speed in knots
+        'fuel_burn_kgph': 240     # Fuel burn in kg per hour
     },
     'descend': {
         'speed_kt': 110,          # Descend speed in knots
-        'fuel_burn_kgph': 200,    # Fuel burn during descend in kg per hour
-        'descend_rate_fpm': 1000  # Descend rate in feet per minute
+        'fuel_burn_kgph': 220,    # Fuel burn during descend in kg per hour
+        'descend_rate_fpm': 500  # Descend rate in feet per minute
     }
 }
