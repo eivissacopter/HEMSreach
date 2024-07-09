357efbefe5179496ee03ee8428e2fcfcc80ad820
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-07-09 18:08:14 +0200

    Update wind.py

--- wind.py
+++ wind.py
@@ -176,9 +176,8 @@ if selected_base:
                                     st.dataframe(df_converted)
 
                             except (UnicodeDecodeError, ValueError, KeyError, IndexError) as e:
-                                st.error(f"Failed to decode or process the forecast data for {closest_airport['name']} ({closest_airport['icao']}): {e}")
+                            st.warning(f"No forecast file found for airport: {closest_airport['name']} ({closest_airport['icao']}).")
                         else:
-                            st.warning(f
                             st.warning(f"No forecast file found for airport: {closest_airport['name']} ({closest_airport['icao']}).")
                 else:
                     st.error("No closest airport found with available forecast data.")
