877dc9f08addd7e06c5997eb42019db6e5c83252
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-07-10 20:38:52 +0200

    Create e6b.py

---
+++ src/hemsreach/e6b.py
@@ -0,0 +1,8 @@
+def calculate_reachable_airports(location, wind_direction, wind_speed, total_fuel_kg, cruise_altitude_ft):
+    # Function to calculate reachable airports
+    # Replace this with actual calculation logic
+    reachable_airports = [
+        {'name': 'Airport 1', 'distance': 50},
+        {'name': 'Airport 2', 'distance': 75},
+    ]
+    return reachable_airports
