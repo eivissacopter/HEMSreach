411fe4f8e36a8b7517702910135d4accf91f5abb
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-07-11 16:35:38 +0200

    Update layer.py

--- src/hemsreach/layer.py
+++ src/hemsreach/layer.py
@@ -60,9 +60,10 @@ def xml_to_geojson(xml_data, layer_type):
             )
             features.append(feature)
     elif layer_type == 'Lightning':
-        for lightning in root.findall('.//dwd:lightning', ns):
-            lat = float(lightning.find('.//dwd:lat', ns).text)
-            lon = float(lightning.find('.//dwd:lon', ns).text)
+        ns_lightning = {'dwd': 'http://www.flugwetter.de/blitzdaten'}
+        for lightning in root.findall('.//dwd:lightning', ns_lightning):
+            lat = float(lightning.find('.//dwd:lat', ns_lightning).text)
+            lon = float(lightning.find('.//dwd:lon', ns_lightning).text)
             feature = geojson.Feature(
                 geometry=geojson.Point((lon, lat)),
                 properties={"status": "lightning"}
@@ -77,11 +78,11 @@ def style_function(feature):
     status = feature['properties']['status']
     color = {
         "default": "#00000000",  # Transparent for default
-        "1": "#FFFF00",        # Yellow for status 1
-        "2": "#FFA500",        # Orange for status 2
-        "3": "#FF0000",        # Red for status 3
-        "4": "#800080",        # Purple for status 4
-        "reflectivity": "#0000FF",  # Blue for reflectivity
+        "Konvektionsstatus 1 (leicht)": "#FFFF00",  # Yellow for status 1
+        "Konvektionsstatus 2 (moderat/mittel)": "#FFA500",  # Orange for status 2
+        "Konvektionsstatus 3 (stark)": "#FF0000",  # Red for status 3
+        "Konvektionsstatus 4 (extrem)": "#800080",  # Purple for status 4
+        "Reflectivity": "#0000FF",  # Blue for reflectivity
         "lightning": "#FFA500"  # Orange for lightning
     }.get(status, "#00000000")  # Default to transparent if not specified
     
@@ -146,109 +147,3 @@ def add_layers_to_map(m, show_nowcastmix_layer, show_lightning_layer, show_terra
                 add_geojson_to_map(m, geojson_data)
 
     return m
-
-# Sample usage in Streamlit
-if __name__ == "__main__":
-    st.set_page_config(page_title="HEMSreach", page_icon="🚁", layout="wide")
-    
-    def apply_custom_css():
-        st.markdown(
-            """
-            <style>
-            body {
-                background-color: #2e2e2e;
-                color: white;
-            }
-            .reportview-container .main .block-container {
-                padding: 0;
-            }
-            .reportview-container .main {
-                background: none;
-                padding: 0;
-            }
-            .sidebar .sidebar-content {
-                background-color: #3e3e3e;
-            }
-            .fullScreenMap {
-                position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 0;
-            }
-            .stSlider, .stNumberInput, .stTextInput {
-                color: black;
-            }
-            .stNumberInput, .stTextInput {
-                display: inline-block;
-                margin-right: 10px;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True,
-        )
-
-    def set_header():
-        st.markdown(
-            """
-            <h1 style='text-align: center;'>
-                🚁 HEMSreach 🚁
-            </h1>
-            """,
-            unsafe_allow_html=True
-        )
-
-    def create_sidebar(helicopter_bases, airports):
-        with st.sidebar:
-            base_names = [base['name'] for base in helicopter_bases]
-            airport_names = [airport['name'] for airport in airports]
-
-            base_or_airport = st.radio('Select Departure', ['Base', 'Airport'], horizontal=True)
-
-            if base_or_airport == 'Base':
-                selected_base_name = st.selectbox('Select Home Base', base_names)
-                selected_location = next(base for base in helicopter_bases if base['name'] == selected_base_name)
-            else:
-                selected_airport_name = st.selectbox('Select Airport', airport_names)
-                selected_location = next(airport for airport in airports if airport['name'] == selected_airport_name)
-
-            selected_base_elevation = selected_location['elevation_ft']
-
-            st.markdown("")
-            cruise_altitude_ft = st.slider(
-                'Cruise Altitude',
-                min_value=3000, max_value=10000, value=5000, step=1000,
-                format="%d ft"
-            )
-            total_fuel_kg = st.slider(
-                'Total Fuel Upload',
-                min_value=400, max_value=723, value=500, step=50,
-                format="%d kg"
-            )
-
-            selected_time = st.slider("Select time window (hours)", min_value=0, max_value=6, value=1)
-
-            show_terrain_layer = st.checkbox("Terrain")
-            show_nowcastmix_layer = st.checkbox("NowCastMix")
-            show_lightning_layer = st.checkbox("Lightning")
-
-        return selected_location, total_fuel_kg, cruise_altitude_ft, selected_time, show_nowcastmix_layer, show_lightning_layer, show_terrain_layer
-
-    from streamlit_autorefresh import st_autorefresh
-    from wtaloft import get_wind_at_altitude
-    from e6b import get_reachable_airports
-    from performance import H145D2_PERFORMANCE
-    from database import helicopter_bases, airports
-    from streamlit_folium import folium_static
-    import pandas as pd
-
-    # Fetch authentication credentials from secrets
-    data_server = st.secrets["data_server"]
-    auth = (data_server["user"], data_server["password"])
-
-    # Set page configuration and custom CSS
-    apply_custom_css()
-
-    # Add header
-    set_header()
-
-    # Auto-refresh every 30 minutes (1800 seconds)
-    st_autorefresh(interval=1800 * 1000, key="data_refresh")
-
-    # Create sidebar and
